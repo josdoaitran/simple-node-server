0587fcb0af570422e14d984d87cd7618fba71d17
Doai Tran <josdoaitran@gmail.com>
2021-04-02 17:31:07 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,71 @@
+var http = require("http");
+var express = require('express');
+var app = express();
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+const { Pool } = require('pg')
+
+// MySQL connection
+// var connectionString = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     port: '5432',
+//     password: '123456',
+//     database: 'mess_service_channel'
+// });
+
+var connectionString = 'postgres://root:123456@localhost:5432/mess_service_channel'
+
+
+//var connectionString =
+//    'postgres://shoizafidgsclk:5aa84844e16bc84b3b49e67a16e339957d289abafdcdf3ad3f48d1c2b4b2cccf@ec2-174-129-255-35.compute-1.amazonaws.com:5432/d7fu349nrp13qh'
+
+app.use(cors());
+
+
+// const pool = new Pool({ connectionString,ssl: {
+//     rejectUnauthorized: false
+//   } })
+
+const pool = new Pool({ connectionString})
+
+module.exports = { pool }
+
+//Body-parser configuration
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+
+
+
+var server = app.listen(process.env.PORT || 3001);
+
+//rest api to get all results
+app.get('/node', function (req, res) {
+    console.log(req);
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+
+    pool.query('select * from node', function (error, results, fields) {
+        if (error) throw error;
+        res.end(JSON.stringify(results.rows));
+    });
+});
+
+//rest api to get all results
+app.get('/link', function (req, res) {
+    console.log(req);
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+
+    pool.query('select * from link', function (error, results, fields) {
+        if (error) throw error;
+        res.end(JSON.stringify(results.rows));
+    });
+});
